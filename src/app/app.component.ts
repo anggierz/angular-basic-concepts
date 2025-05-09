09b807cb7f8f8c93a9b4bb1a00815b45172378dc
anggierz <anggie.rizo.zambrano@gmail.com>
2025-05-09 19:43:24 +0200

    add user form component functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ListComponent } from './components/list/list.component';
 import { CounterComponent } from './components/counter/counter.component';
+import { FormComponent } from './components/form/form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ListComponent, CounterComponent],
+  imports: [RouterOutlet, ListComponent, CounterComponent, FormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, Form } from '@angular/forms';
+import { NgIf, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, NgIf, NgFor],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss'
 })
 export class FormComponent {
+  userForm: FormGroup;
+  users: any[] = [];
 
+  constructor(private formBuilder: FormBuilder) {
+    this.userForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.maxLength(100)]],
+      lastname: ['', [Validators.required, Validators.maxLength(100)]],
+      telephone: ['', Validators.required],
+      birthdate: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.userForm.valid) {
+      this.users.push(this.userForm.value);
+      this.userForm.reset();
+      console.log(this.users);
+    } else {
+      alert('Please fill in all fields correctly.');
+    }
+  }
 }
