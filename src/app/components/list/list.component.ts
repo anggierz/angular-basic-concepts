fe11f81bfe68693bc2b17934896d89e098e19b13
anggierz <anggie.rizo.zambrano@gmail.com>
2025-05-09 18:46:44 +0200

    list component: add and delete items

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [NgFor, FormsModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
 export class ListComponent {
+  list: string[] = [];
+  newElement: string = "";
+
+  constructor() {}
+
+  addElement() {
+    if (this.newElement?.trim() !== "") {
+      this.list.push(this.newElement);
+      this.newElement = "";
+    }
+  }
+
+  removeElement(index: number) {
+    this.list.splice(index, 1);
+  }
 
 }
